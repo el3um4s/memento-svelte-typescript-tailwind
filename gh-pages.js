fb597cabba13a3a26a20b0c54be722fb4e0842fb
Samuele de Tomasi <el3um4s@gmail.com>
2022-01-06 13:28:14 +0100

    updated link

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ import {
 } from 'gh-pages';
 
 publish(
-    'build', // path to public directory
+    'public', // path to public directory
     {
         branch: 'gh-pages',
         repo: 'https://github.com/el3um4s/memento-svelte-typescript-tailwind.git', // Update to point to your repository
--- src/App.svelte
+++ src/App.svelte
@@ -10,4 +10,5 @@
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
   </p>
+  <p>Version: 0.0.1</p>
 </main>
